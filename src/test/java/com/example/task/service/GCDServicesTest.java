ba1922e22d1bbe34a0c7a09bb50935ad6bbc18e8
murazyan <murazyan.margarita@mail.ru>
2020-01-25 01:21:47 +0400

    test finished

--- src/test/java/com/example/task/service/GCDServicesTest.java
+++ src/test/java/com/example/task/service/GCDServicesTest.java
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 public class GCDServicesTest extends AbstractTestNGSpringContextTests {
 
+
     @TestConfiguration
     static class GCDServiceTestContextConfiguration {
 
